7219f4b28d682dffcfd446524d08317a58f4080f
Ghanshyam Mann <gmann@ghanshyammann.com>
2019-12-31 18:57:46 +0000

    Add test coverage of existing admin_password policies-2

--- nova/tests/unit/policies/test_admin_password.py
+++ nova/tests/unit/policies/test_admin_password.py
@@ -40,19 +40,20 @@ class AdminPasswordPolicyTest(base.BasePolicyTest):
         uuid = uuids.fake_id
         self.instance = fake_instance.fake_instance_obj(
                 self.project_member_context,
-                id=1, uuid=uuid, vm_state=vm_states.ACTIVE,
+                id=1, uuid=uuid, project_id=self.project_id,
+                vm_state=vm_states.ACTIVE,
                 task_state=None, launched_at=timeutils.utcnow())
         self.mock_get.return_value = self.instance
-        # Check that admin is able to change the password
+        # Check that admin and server owner is able to change the password
         self.admin_authorized_contexts = [
             self.legacy_admin_context, self.system_admin_context,
-            self.project_admin_context]
+            self.project_admin_context, self.project_member_context,
+            self.project_reader_context, self.project_foo_context]
         # Check that non-admin is not able to change the password
         self.admin_unauthorized_contexts = [
             self.system_member_context, self.system_reader_context,
-            self.system_foo_context, self.project_member_context,
-            self.other_project_member_context,
-            self.project_foo_context, self.project_reader_context
+            self.system_foo_context,
+            self.other_project_member_context
         ]
 
     @mock.patch('nova.compute.api.API.set_admin_password')
